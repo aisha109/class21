7190a42b333536de196639b0f48f46b164ad27e7
aisha109 <62790685+aisha109@users.noreply.github.com>
2020-04-04 17:58:05 +0300

    Add files via upload

--- p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -1,24 +1,35 @@
-var fixedRect,object1;
+var Rect1;
 
 
 function setup() {
   createCanvas(800,800);
-  Rect1 = createSprite(200, 100, 50, 50);
-  Rect2 = createSprite(200,350,50,100);
+
+  Rect1 = createSprite(400,350,50,50);
   
-  Rect1.velocityY = 4;
- Rect2.velocityY =-3; 
+
 }
 
 function draw() {
-  background("black");  
+  background ("white");
 
 
+  Rect1.x = World.mouseX;
+  Rect1.y = World.mouseY;
 
+  if (Rect1.x >200 || Rect1.y>200){
 
- 
+  background("pink");
 
-JumpOff (Rect1,Rect2);
+  }
 
+  if (Rect1.x<300||Rect1.y < 300 ){
+
+    background("yellow");
+  }
+
+if (Rect1.x<100|| Rect1.y <100 ){
+
+  background("red");
+}
   drawSprites();
 }
